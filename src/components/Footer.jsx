907a1e4edc99d4ac3e7c9881ca9486b8c9806f9c
varunrout <varun_rout@outlook.com>
2024-05-17 00:58:06 +0100

    Navbar and Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
-const Footer = () => {
-    return (
-        <></>
-    )
-}
+// src/components/Footer.js
+import React from 'react';
+import '../assets/Stylesheets/Footer.css';
 
-export default Footer
+const Footer = () => (
+    <footer className="footer">
+        <div className="footer-content">
+            <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+            <div className="social-links">
+                <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <a href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub</a>
+                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+            </div>
+        </div>
+    </footer>
+);
+
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
-const Navbar = () => {
-    return (
-        <></>
-    )
-}
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/Stylesheets/Navbar.css';
 
-export default Navbar
+const Navbar = () => (
+    <nav className="navbar">
+        <div className="navbar-brand">
+            <Link to="/">MyPortfolio</Link>
+        </div>
+        <ul className="navbar-links">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/portfolio">Portfolio</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+        </ul>
+    </nav>
+);
+
+export default Navbar;
